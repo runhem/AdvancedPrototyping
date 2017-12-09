766f47a12cdb4701bbb8030f2fb1a13d7dea9e2e
Mathias Peluchon <mathias.peluchon@laposte.net>
2017-12-09 21:34:01 +0100

    pins definitions and low level function impl.
    Definition of needed pins as well as some low level function for driving
    motors and using sensors

--- SourceCode/SourceCode.ino
+++ SourceCode/SourceCode.ino
@@ -1,6 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
+#define TBD 0 
 
+//Connections
+//Ultrasonic sensor
+usTriggerPin = TBD;
+usReadPin = TBD;
+
+//Servo
+servoPwmPin = TBD;
+
+//DC motor (through H-bridge)
+dcPwmPin = TBD;
+
+//Line sensor
+leftLinePin = TBD;
+midLinePin = TBD;
+rightLinePin = TBD;
+
+
+
+Servo servo;
+
+
+
+void setup() {
+  setupDcMotor();
+  setupUsSensor();
+  setupServo();
 }
 
 void loop() {
@@ -9,9 +35,61 @@ void loop() {
 }
 
 
+
+
+
+void setupDcMotor() {
+      //pinMode(buttonPin, INPUT); 
+//      pinMode(inaPin, OUTPUT);
+//      pinMode(inbPin, OUTPUT);
+      pinMode(dcPwmPin, OUTPUT);
+//      pinMode(diagaPin, INPUT);
+//      pinMode(diagbPin, INPUT);
+//      pinMode(trimPin, INPUT);
+      }
+
+void setupUsSensor(){
+pinMode(usTriggerPin, OUTPUT);
+pinMode(usReadPin, INPUT);
+}
+
+void setupServo(){
+  servo.attach(servoPwmPin);
+  }
+
+
+
+int readUsSensor(){
+      long duration, distance;
+      digitalWrite(trigPin, LOW);  // Added this line
+      delayMicroseconds(2); // Added this line
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10); // Added this line
+      digitalWrite(trigPin, LOW);
+      duration = pulseIn(echoPin, HIGH);
+      distance = (duration/2) / 29.1;
+      return distance;
+}
+
+
+void setServoPos(int currentAngle,int newAngle)
+{
+  rotationSpeed = 110; //90 is 0
+  nbTurnsPerS = 7;//how many turn per second the servo does at rotationSpeed
+  deltaAngle = currentAngle - newAngle;
+  
+  servo.write(rotationSpeed);
+  delayMicroSeconds(1000*1000*deltaAngle/360/nbTurnsPerS);
+  servo.write(90);//Stop
+}
+
+
+
+  
+
 //Function to controll the DC Motor
 //ONLY TEST CODE in function. Change accordingly when assembling
-void motorCtrl(){
+//void motorCtrl(){
   
     //const int inaPin = 13;
     //const int inbPin = 9;
@@ -96,4 +174,30 @@ void ultraSonCtrl(){
     //}
 
 }
+
+
+//SERVO control example
+//#include <Servo.h>
+//
+//Servo myservo;  // create servo object to control a servo
+//// twelve servo objects can be created on most boards
+//
+//int pos = 0;    // variable to store the servo position
+//
+//void setup() {
+//  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+//}
+//
+//void loop() {
+//  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+//    // in steps of 1 degree
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(15);                       // waits 15ms for the servo to reach the position
+//  }
+//  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+//    delay(15);                       // waits 15ms for the servo to reach the position
+//  }
+//}
+//
 
