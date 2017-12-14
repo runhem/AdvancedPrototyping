78d44086f8b8827a7bb20876ad9957c7192900c3
runhem <you@example.com>
2017-12-14 11:14:11 +0100

    Structure of obstacle avoidance, follow the line is now a functions that is called in the loop

--- SourceCode/SourceCode.ino
+++ SourceCode/SourceCode.ino
@@ -11,9 +11,9 @@
   
   
   //DC motor (through H-bridge)
-  int dcPwmPin = 11;
-  int dcInAPin = 13;
-  int dcInBPin = 8;
+  int dcPwmPin = 11; //Pin 6 on H-bridge
+  int dcInAPin = 13; //Pin 4 on H-bridge
+  int dcInBPin = 8; // Pin 5 on H-bridge
   
   //Line sensor
   int leftLinePin = A5;
@@ -41,12 +41,16 @@
       far,
       veryFar
   };
-  
+
+  //---Set Up----
   void setup() { 
     lineSensor = malloc(3*sizeof(int));
     lineSensor[0] = 0;
     lineSensor[1] = 0;
     lineSensor[2] = 0;
+    digitalWrite(dcInAPin, HIGH);
+    digitalWrite(dcInBPin, LOW);
+    analogWrite(dcPwmPin, 150);
     
     Serial.begin(9600);
 
@@ -58,8 +62,28 @@
 
   }
 
+  //---END OF Set Up----
+
+  //---LOOP----
+
   void loop() 
   {
+    //Check distance to obstacle
+    int distanceToCollision = readUsSensor();
+
+    //If distance to obstacle is too close, run obstacle avoidance protocol
+    if (distanceToCollision == tooClose)
+    {
+      Serial.println("too close");
+      obstacleAvoidance();
+    }
+    //If distance to obstacle is far enough, follow the line
+    else if (distanceToCollision == farEnough){
+      Serial.println("Far enough");
+      followLineProtocol();
+    }
+    delay(500);
+    
     /*Serial.println(analogRead(leftLinePin));
     Serial.println(analogRead(midLinePin));
     Serial.println(analogRead(rightLinePin));
@@ -67,77 +91,11 @@
     //readLineSensor(lineSensor,lineSensor);
     /*Serial.println(lineSensor[0]);
     Serial.println(lineSensor[1]);
-    Serial.println(lineSensor[2]);*/ 
-  
-    digitalWrite(dcInAPin, HIGH);
-    digitalWrite(dcInBPin, LOW);
-    analogWrite(dcPwmPin, 150);
-    
-    int linePos = linePosition();  
-    //Line following if no obstacle
-    if(linePos == left)
-    {
-      Serial.println(linePos);
-      Serial.println(left);
-      Serial.println("Line to the left");
-      setServoPos(0,20);
-      delay(1000);
-      setServoPos(20,0);
-      linePos = linePosition();
-      
-      while(linePos != middle)
-        {
-          delay(500);
-          linePos = linePosition();
-        }
-          
-      setServoPos(0,-20);//return in initial direction
-      delay(1000);
-      setServoPos(-20,0);
-    }
-    
-    else if(linePos == right)
-    {
-      Serial.println(linePos);
-      Serial.println(right);
-      Serial.println("Line to the right");
-      setServoPos(0,-20);
-      delay(1000);
-      setServoPos(-20,0);
-      linePos = linePosition();
-      
-      while(linePos != middle)
-      {
-        delay(500);
-        linePos = linePosition();
-      }
-      
-      setServoPos(0,20);//return in initial direction
-      delay(1000);
-      setServoPos(20,0);
-    }
-
-    int distanceToCollision = readUsSensor();
-
-    if (distanceToCollision == tooClose){
-      Serial.println("too close");
-      //Call obstical avoidence function      
-      
-    }
-
-    else if (distanceToCollision == farEnough){
-      Serial.println("Far enough");
-      //Call line following proc.
-    }
-
-    else if (distanceToCollision == far){
-      
-    }
-    
-  delay(500); 
-   readUsSensor();
+    Serial.println(lineSensor[2]);*/  
   
   }
+
+//---END OF Loop----
 
 //---SETUP FUNCTIONS---
   void setupDcMotor() 
@@ -202,8 +160,7 @@
     else{
       return veryFar;
     }
-    
-    delay(500);
+   
   }
 
 
@@ -216,13 +173,14 @@
     if(deltaAngle > 0)
     {
       servo.write(80);
+      
       delay(float(1000*abs(deltaAngle))/360/nbTurnsPerSPositive);
+      
     }
     else if (deltaAngle < 0)
     {
       servo.write(100);
       delay(float(1000*abs(deltaAngle))/360/nbTurnsPerSNegative);
-      //Serial.println((float(1000*abs(deltaAngle))/360/nbTurnsPerSNegative));
     }
     
     servo.write(90);//Stop
@@ -255,10 +213,75 @@
   newStates[1] = analogRead(midLinePin);//previousStates[1] + lowPassAlpha * (newStates[1] - analogRead(midLinePin));
   newStates[2] = analogRead(rightLinePin);//previousStates[2] + lowPassAlpha * (newStates[2] - analogRead(rightLinePin));
   //If measurements are not stable enough, add another block like this with a small delay
-  
   //return sensorStates;
   }
 
+
+  void followLineProtocol(){
+    Serial.println("inne i lineprot");
+    int linePos = linePosition(); 
+    Serial.println(linePos); 
+    //Line following if no obstacle
+    if(linePos == left)
+    {
+      Serial.println(linePos);
+      Serial.println(left);
+      Serial.println("Line to the left");
+      setServoPos(0,20);
+      delay(1000);
+      setServoPos(20,0);
+      linePos = linePosition();
+      
+      while(linePos != middle)
+        {
+          delay(500);
+          linePos = linePosition();
+        }
+          
+      setServoPos(0,-20);//return in initial direction
+      delay(1000);
+      setServoPos(-20,0);
+    }
+    
+    else if(linePos == right)
+    {
+      Serial.println(linePos);
+      Serial.println(right);
+      Serial.println("Line to the right");
+      setServoPos(0,-20);
+      delay(1000);
+      setServoPos(-20,0);
+      linePos = linePosition();
+      
+      while(linePos != middle)
+      {
+        delay(500);
+        linePos = linePosition();
+      }
+      
+      setServoPos(0,20);//return in initial direction
+      delay(1000);
+      setServoPos(20,0);
+    }
+  }
+  int count = 0; //Used to check if it's the first or second time we call obstacle avoidance (); Maybe have to move!
+  
+  void obstacleAvoidance(){
+    Serial.println("Count = ");
+    Serial.println(count);
+    if(count = 0){
+      //Make hard coded track
+      count = 1;
+      Serial.println("Count = ");
+      Serial.println(count);
+    }
+
+    else{ //Second time we lose the line
+      //STOP. Probably at the finish line
+    }
+    
+    
+  }
 
 
 /*int linePosition()
@@ -294,19 +317,27 @@
     
     if(isWhiter(leftMeasure,midMeasure) && isWhiter(rightMeasure,midMeasure))
      {
+      Serial.println("middle");
       return middle;//Line is on the middle sensor
+      
      }
     else if(isWhiter(leftMeasure,rightMeasure))
      {
+      Serial.println("right");
       return right; //line is on the right sensor
+      
      }
     else if(isWhiter(rightMeasure, leftMeasure))
      {
+      Serial.println("left");
       return left; //line is on the left sensor
+      
      }
     else
     {
+      Serial.println("do nooo");
       return unknown;//Line lost
+      
     }
   }
 
@@ -320,101 +351,4 @@
   }
 
 
-//void ultraSonCtrl(){
-//  
-//    long duration, distance;
-//    digitalWrite(trigPin, LOW); 
-//    delayMicroseconds(2); 
-//    digitalWrite(trigPin, HIGH);
-//    delayMicroseconds(1000); 
-//    digitalWrite(trigPin, LOW);
-//    duration = pulseIn(echoPin, HIGH);
-//    if (distance < 10) { 
-//      Serial.println("CLOSE");
-//    }
-//
-//    else{
-//      Serial.println("No worries");
-//    }
-//    
-//    if (distance >= 200 || distance <= 0){
-//    Serial.println("Out of range");
-//    }
-//    else {
-//      Serial.print(distance);
-//      Serial.println(" cm");
-//    }
-//    delay(500);
-//    }
-//}
-
-
-
-//Function to control the DC Motor
-//ONLY TEST CODE in function. Change accordingly when assembling
-//void motorCtrl(){
-  
-    //const int inaPin = 13;
-    //const int inbPin = 9;
-    //const int pwmPin = 11;
-    //const int diagaPin = 10;
-    //const int diagbPin = 12;
-    //const int buttonPin = 2;
-    //const int trimPin = A0;
-    //
-    //int i = 0;
-    //
-    //void setup() {
-    //  pinMode(buttonPin, INPUT); 
-    //  pinMode(inaPin, OUTPUT);
-    //  pinMode(inbPin, OUTPUT);
-    //  pinMode(pwmPin, OUTPUT);
-    //  pinMode(diagaPin, INPUT);
-    //  pinMode(diagbPin, INPUT);
-    //  pinMode(trimPin, INPUT);
-    // 
-    //}
-    //
-    //void loop() {
-    //  
-    //  // CW
-    //  digitalWrite(inaPin, HIGH);
-    //  digitalWrite(inbPin, LOW);
-    //  while(HIGH == HIGH){
-    //    i = 1023;
-    //    analogWrite(pwmPin, map(i, 0, 1023, 0, 255));
-    //
-    //  }
-    //
-    //  
-    //}
-  
-//}
-
-
-
-//SERVO control example
-//#include <Servo.h>
-//
-//Servo myservo;  // create servo object to control a servo
-//// twelve servo objects can be created on most boards
-//
-//int pos = 0;    // variable to store the servo position
-//
-//void setup() {
-//  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
-//}
-//
-//void loop() {
-//  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-//  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-//    delay(15);                       // waits 15ms for the servo to reach the position
-//  }
-//}
-//
 
